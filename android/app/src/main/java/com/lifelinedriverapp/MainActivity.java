d5aa39b0c2a18ccc06f685f3d96963c1aab9f8c1
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-16 21:50:55 +0545

    Correct error in MainActivity.java

--- android/app/src/main/java/com/lifelinedriverapp/MainActivity.java
+++ android/app/src/main/java/com/lifelinedriverapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.lifelinedriverapp;
 import com.facebook.react.ReactActivity;
 
 // react-native-splash-screen >= 0.3.1 
+import android.os.Bundle; // here 
 import org.devio.rn.splashscreen.SplashScreen; // here 
 
 public class MainActivity extends ReactActivity {
