cfbedfe8aaf265b8dd06fe92103fa4f1ba583da0
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-16 22:18:55 +0545

    Reading from .env

--- src/components/Test/index.js
+++ src/components/Test/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+// env
+import { API_URL } from '@env';
+
 function Test() {
   return (
     <View>
-      <Text>Hello from Text</Text>
+      <Text>API URL: {API_URL}</Text>
     </View>
   );
 }
