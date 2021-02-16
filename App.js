2a8861b7e6e21fea1e4d78f48b7ebd3a7b277444
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-16 22:13:16 +0545

    Working absolute pasth

--- App.js
+++ App.js
@@ -12,14 +12,18 @@ import { View, Text, StatusBar } from 'react-native';
 // packages
 import SplashScreen from 'react-native-splash-screen';
 
+// components
+import Test from 'components/Test';
+
 function App() {
   useEffect(() => SplashScreen.hide(), []);
 
   return (
     <>
-      <StatusBar backgroundColor='#ffffff' barStyle='dark-content' />
+      <StatusBar backgroundColor='#000000' barStyle='light-content' />
       <View>
         <Text>Hello World</Text>
+        <Test />
       </View>
     </>
   );
