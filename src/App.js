5d3ebca00d79fef0053d19669eb024ab618bbba5
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-17 12:38:19 +0545

    Update App

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 // packages
+import MapboxGL from '@react-native-mapbox-gl/maps';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 // navigator
@@ -28,6 +29,9 @@ import UserInfo from 'utils/userInfo';
 import Fonts from 'global/fonts';
 import Colors from 'global/colors';
 
+// env
+import { MAPBOX_API_KEY } from '@env';
+
 const theme = {
   ...DefaultTheme,
   colors: {
@@ -40,6 +44,8 @@ const theme = {
     regular: { fontFamily: Fonts.regular }
   }
 };
+
+MapboxGL.setAccessToken(MAPBOX_API_KEY);
 
 function App() {
   const [isReady, setIsReady] = useState(false);
