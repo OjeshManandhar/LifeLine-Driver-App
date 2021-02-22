a88276f687cff85901c73337a46cc31f65dd449d
OjeshManandhar <ozes.manandhar@gmail.com>
2021-02-22 10:30:29 +0545

    Update for token check

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,17 +1,17 @@
 // packages
 import io from 'socket.io-client';
 
-// utils
-import UserInfo from 'utils/userInfo';
-
 // env
 import { SOCKET_ENDPOINT } from '@env';
+
 class Socket {
   #alreadyInit = false;
   #socket = null;
 
   init() {
     if (this.#alreadyInit) return;
+
+    // Add token here
 
     this.#socket = io(SOCKET_ENDPOINT, {
       autoConnect: true,
